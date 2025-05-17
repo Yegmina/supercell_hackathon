a9028324a9160477580f6ad0242cdc728d8a1cfd
Arthur Roos <artur.roos@ktnlvr.dev>
2025-05-17 16:14:13 +0300

    Move the witch randomly between positions

--- supercell_hackathon-main/Assets/Scripts/PathNetwork.cs
+++ supercell_hackathon-main/Assets/Scripts/PathNetwork.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
-public struct Edge
+public class Edge
 {
     public string start;
     public string end;
@@ -18,10 +19,13 @@ public class PathNetwork : MonoBehaviour
     public static PathNetwork singleton;
     [SerializeField]
     public List<Edge> edges;
+    public Dictionary<string, GameObject> children = new Dictionary<string, GameObject>();
 
     void Start()
     {
         singleton = this;
+        foreach (Transform child in transform)
+            children[child.name] = child.gameObject;
     }
 
     void Update()
@@ -31,7 +35,6 @@ public class PathNetwork : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        var children = new Dictionary<string, GameObject>();
         Gizmos.color = Color.white;
         foreach (Transform child in transform)
         {
@@ -51,5 +54,34 @@ public class PathNetwork : MonoBehaviour
 
             Gizmos.DrawLine(start.transform.position, end.transform.position);
         }
+    }
+
+    public string GetRandomAdjacentNode(string nodeName)
+    {
+        List<string> adjacentNodeNames = new List<string>();
+
+        foreach (Edge edge in edges)
+        {
+            if (!edge.enabled) continue;
+
+            if (edge.start == nodeName)
+            {
+                adjacentNodeNames.Add(edge.end);
+            }
+            else if (edge.end == nodeName)
+            {
+                adjacentNodeNames.Add(edge.start);
+            }
+        }
+
+        if (adjacentNodeNames.Count == 0)
+        {
+            return null; // No adjacent nodes found
+        }
+
+        string randomAdjacentName = adjacentNodeNames[UnityEngine.Random.Range(0, adjacentNodeNames.Count)];
+
+        children.TryGetValue(randomAdjacentName, out GameObject adjacentNode);
+        return adjacentNode.name;
     }
 }
--- supercell_hackathon-main/Assets/Scripts/WitchController.cs
+++ supercell_hackathon-main/Assets/Scripts/WitchController.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
 
+public enum State
+{
+    Standing,
+    Walking,
+    Petting,
+    OpeningDoor,
+    PuttingFood,
+}
+
 public class WitchController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public PathNetwork network;
+    public string currentNode;
+    public State state = State.Standing;
+
+    public float timeLeft;
+    public float moveSpeed = 2f;
+
     void Start()
     {
-        
+        Edge edge = network.edges[0];
+        currentNode = edge.start;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        var node = network.children[currentNode];
+
+        if (state == State.Standing)
+        {
+            if (timeLeft < 0)
+            {
+                var newNode = network.GetRandomAdjacentNode(currentNode);
+                Debug.Log(newNode);
+                currentNode = newNode;
+                state = State.Walking;
+            }
+        }
+        else if (state == State.Walking)
+        {
+            timeLeft = 2f;
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                node.transform.position,
+                moveSpeed * Time.deltaTime
+            );
+
+            if (Vector3.Distance(transform.position, node.transform.position) < 0.2)
+            {
+                transform.position = node.transform.position;
+
+                currentNode = node.name;
+                state = State.Standing;
+                timeLeft = Random.Range(2f, 5f);
+            }
+        }
     }
 }
